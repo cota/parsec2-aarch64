0f51444defe4bd1d5503c5257d35a9efd0aed42b
Emilio G. Cota <cota@braap.org>
2018-06-04 10:36:42 -0400

    bugfix: Data race in blackscholes OpenMP version
    Author: Paul Keir
    Date: May 4, 2010
    
    Signed-off-by: Emilio G. Cota <cota@braap.org>

--- pkgs/apps/blackscholes/src/blackscholes.c
+++ pkgs/apps/blackscholes/src/blackscholes.c
@@ -232,7 +232,7 @@ int bs_thread(void *tid_ptr) {
 
     for (j=0; j<NUM_RUNS; j++) {
 #ifdef ENABLE_OPENMP
-#pragma omp parallel for
+#pragma omp parallel for private(i, price, priceDelta)
         for (i=0; i<numOptions; i++) {
 #else  //ENABLE_OPENMP
         for (i=start; i<end; i++) {
