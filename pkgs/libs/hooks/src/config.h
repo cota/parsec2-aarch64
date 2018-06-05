98e9ddfc7f9317bca490120dab14a49f99a8c145
Emilio G. Cota <cota@braap.org>
2018-06-05 17:28:04 -0600

    hooks: disable affinity
    Signed-off-by: Emilio G. Cota <cota@braap.org>

--- pkgs/libs/hooks/src/config.h
+++ pkgs/libs/hooks/src/config.h
@@ -71,7 +71,7 @@
  * This functionality is enabled by default on Linux systems.
  */
 //NOTE: Requires sched_setaffinity() (Linux only)
-#define ENABLE_SETAFFINITY 1
+#define ENABLE_SETAFFINITY 0
 /** \brief Name of environment variable to control number of CPUs.
  *
  * This macro defines the name of one of the two environment variables which
